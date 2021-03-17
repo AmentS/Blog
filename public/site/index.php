67166eb6d09bd44b3c101c7cc7c1633cfe27b486
Spasoje Djurovic <spasoje.djurovic90@gmail.com>
2021-03-17 12:32:57 +0100

    Add frontend blog listing

--- public/site/index.php
+++ public/site/index.php
@@ -4,19 +4,85 @@
 ?>
 
 <?php include_once '../../views/partials/header.php' ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="container-2">
+    <div class="wrap">
+        <div class="container-2">
+            <div class="container-1">
+                <div class="container-2-box">
+                    <select name="" id="" class="sort-blog">
+                        <option value="">Sortiraj po:</option>
+                        <option value="">Najnoviji</option>
+                        <option value="">Prvi ikad</option>
+                    </select>
+                </div>
+            </div>
+            <div class="art-container margin-b-3">
+                <div class="art-container-box-l" style="background-image: url('../img/whatisblog.png')"></div>
+                <div class="art-container-box-r">
+                    <p class="color-gray font-1">Datum: 21.12.2020</p>
+                    <h3 class="heading-3 color-org margin-t-1">Prvi blog</h3>
+                    <p class="color-gray heading-1 margin-t-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                        consectetur
+                        tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                        ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                        eget
+                        risus nec, dapibus efficitur odio.
+                    </p>
+                    <p class="color-gray heading-1 margin-t-2"><a href="" class="procitaj">Procitaj vise...</a></p>
+                </div>
+            </div>
+            <div class="art-container margin-b-3">
+                <div class="art-container-box-l" style="background-image: url('../img/whatisblog.png')"></div>
+                <div class="art-container-box-r">
+                    <p class="color-gray font-1">Datum: 21.12.2020</p>
+                    <h3 class="heading-3 color-org margin-t-1">Drugi blog</h3>
+                    <p class="color-gray heading-1 margin-t-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                        consectetur
+                        tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                        ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                        eget
+                        risus nec, dapibus efficitur odio.
+                    </p>
+                    <p class="color-gray heading-1 margin-t-2"><a href="" class="procitaj">Procitaj vise...</a></p>
+                </div>
+            </div>
+            <div class="art-container margin-b-3">
+                <div class="art-container-box-l" style="background-image: url('../img/whatisblog.png')"></div>
+                <div class="art-container-box-r">
+                    <p class="color-gray font-1">Datum: 21.12.2020</p>
+                    <h3 class="heading-3 color-org margin-t-1">Treci blog</h3>
+                    <p class="color-gray heading-1 margin-t-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                        consectetur
+                        tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                        ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                        eget
+                        risus nec, dapibus efficitur odio.
+                    </p>
+                    <p class="color-gray heading-1 margin-t-2"><a href="" class="procitaj">Procitaj vise...</a></p>
+                </div>
+            </div>
+            <div class="art-container margin-b-3">
+                <div class="art-container-box-l" style="background-image: url('../img/whatisblog.png')"></div>
+                <div class="art-container-box-r">
+                    <p class="color-gray font-1">Datum: 21.12.2020</p>
+                    <h3 class="heading-3 color-org margin-t-1">Cetvrti blog</h3>
+                    <p class="color-gray heading-1 margin-t-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                        consectetur
+                        tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                        ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                        eget
+                        risus nec, dapibus efficitur odio.
+                    </p>
+                    <p class="color-gray heading-1 margin-t-2"><a href="" class="procitaj">Procitaj vise...</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php include '../../views/partials/footer.php' ?>
