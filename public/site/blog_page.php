9b277abeebe86793a47456758f624409c31dfa17
Spasoje Djurovic <spasoje.djurovic90@gmail.com>
2021-03-22 16:00:50 +0100

    Add comment section in blog page

--- public/site/blog_page.php
+++ public/site/blog_page.php
@@ -11,10 +11,13 @@
             <div class="blog-page-img" style="background-image: url('../img/blog-winter-img-2.jpg')"></div>
         </div>
         <div class="container-2">
-            <h1 class="heading-5 color-org">Blog prvi</h1>
+            <div class="container-1">
+                <h1 class="heading-5 color-org">Blog prvi</h1>
+            </div>
             <div class="content-blog-page">
                 <p class="font-1 color-gray margin-t-1">Datum: 23.12.2020</p>
-                <p class="heading-1 color-gray margin-t-1">Kategorija: <strong class="color-org">nauka, tehnologija, programiranje</strong></p>
+                <p class="heading-1 color-gray margin-t-1">Kategorija: <strong class="color-org">nauka, tehnologija,
+                        programiranje</strong></p>
                 <p class="heading-1 color-gray margin-t-3">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
                     consectetur
@@ -37,6 +40,38 @@
                 </p>
             </div>
 
+
+        </div>
+        <div class="container-2 margin-t-5 border-bottom-gr">
+            <p class="heading-1 color-gray">Comments:</p>
+        </div>
+
+        <div class="container-2">
+            <div class="coomment-read margin-t-3">
+                <p class="font-2 f-w-b color-gray">John Doe:</p>
+                <p class="heading-1 color-gray f-w-l margin-t-1">risus nec, dapibus efficitur odio.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.</p>
+            </div>
+            <div class="coomment-read margin-t-3">
+                <p class="font-2 f-w-b color-gray">John Doe:</p>
+                <p class="heading-1 color-gray f-w-l margin-t-1">risus nec, dapibus efficitur odio.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.</p>
+            </div>
+
+            <div class="coomment-write margin-t-5">
+                <textarea class="textarea-comment" placeholder="Enter your comment here..."></textarea>
+                <button class="float-right margin-t-1 comment-post-btn">Submit</button>
+            </div>
         </div>
     </div>
 
