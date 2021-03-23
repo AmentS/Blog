28155b4ce2bea5481e8e112c7d02c5a5591ed1f0
Spasoje Djurovic <spasoje.djurovic90@gmail.com>
2021-03-23 09:08:44 +0100

    Add frontend about page

--- public/site/about.php
+++ public/site/about.php
@@ -1,4 +1,3 @@
-
 <?php include_once '../../views/partials/header.php' ?>
 <div class="container-2">
     <div class="wrap">
@@ -11,25 +10,68 @@
                 <h1 class="heading-4 color-gray f-w-b">About me</h1>
 
             </div>
-            <div class="container-2-box border-r">
+            <div class="container-2-box">
                 <h2 class="heading-2 color-org margin-t-3">In general</h2>
                 <p class="heading-1 color-gray margin-t-2"><strong>Name:</strong> Spasoje Djurovic</p>
                 <p class="heading-1 color-gray margin-t-1"><strong>Date of birth:</strong> 23.12.1990</p>
                 <p class="heading-1 color-gray margin-t-1"><strong>Born in: </strong> Kotor, Montenegro</p>
-                <p class="heading-1 color-gray margin-t-1"><strong>Hometown: </strong> Herceg Novi, Srbina 43, Montenegro</p>
+                <p class="heading-1 color-gray margin-t-1"><strong>Hometown: </strong> Herceg Novi, Srbina 43,
+                    Montenegro</p>
                 <p class="heading-1 color-gray margin-t-1"><strong>E-mail:</strong> spasoje.djurovic90@gmail.com</p>
                 <p class="heading-1 color-gray margin-t-1"><strong>Phone: </strong> 069/453-711</p>
                 <h2 class="heading-2 color-org margin-t-3">Education</h2>
+                <p class="heading-1 color-gray margin-t-2"><strong>Osnovna škola: </strong>Dašo Pavičić (1997 - 2005)
+                </p>
+                <p class="heading-1 color-gray margin-t-2"><strong>Srednja škola: </strong>Ivan Goran Kovičić (2005 -
+                    2009)</p>
+                <p class="heading-1 color-gray margin-t-2"><strong>Fakultet: </strong>Pomorski Fakultet Kotor, smjer:
+                    Menadžment u pomorstvu (2009 - 2015)</p>
+                <p class="heading-1 color-gray margin-t-2"><strong>Fakultet: </strong>Univerzitet Mediteran, smjer:
+                    Softverski inženjering (2016 - 2020)</p>
+                <h2 class="heading-2 color-org margin-t-3">Radno iskustvo</h2>
+                <p class="heading-1 color-gray margin-t-2"><strong>MDDX </strong>Septembar, 2015 - Septembar, 2016 <br>Testiranje
+                    softvera, pregled maskiranje i slanje snimaka u laboratoriji,
+                    rešavanje query-ja, olaniranje i izrada grupnih projekata </p>
+                <p class="heading-1 color-gray margin-t-2"><strong>Reach </strong>Mart, 2019 - Maj, 2020 <br>Rad sa
+                    Google Spreadsheet-om, planiranje i izrada zadataka, rads a klijentima, tehnicka podrska</p>
+                <p class="heading-1 color-gray margin-t-2"><strong>Web centar </strong>Septembar, 2020 - <br>Web
+                    developer</p>
+
             </div>
-            <div class="container-2-box border-r">
+            <div class="container-2-box">
+                <p class="heading-1 color-gray "><strong class="color-org">Lorem ipsum dolor</strong> sit amet,
+                    consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.
+                    sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.
+                    sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.
+                    sit amet, consectetur adipiscing elit. Aliquam lacinia justo nisi, vel
+                    consectetur
+                    tortor sodales at. In nec feugiat diam, eu dictum risus. Vivamus ut venenatis neque, at congue
+                    ligula. Fusce porta ex erat, nec blandit sem sollicitudin vel. Praesent diam diam, tincidunt
+                    eget
+                    risus nec, dapibus efficitur odio.</p>
 
             </div>
 
         </div>
     </div>
 
-</div>
 
+</div>
 
 
 <?php include '../../views/partials/footer.php' ?>
